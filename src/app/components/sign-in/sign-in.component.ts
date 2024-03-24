bb770eb146faf31b724951faf1152e17ad59fbfc
returnT0 <vlvolkov25@gmail.com>
2024-03-24 21:49:02 +0100

    added basic design sign-in/up

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit, OnDestroy {
+  usernamePattern = /^[a-zA-Z]{2,}$/;
+  emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/; // min 6 chars, at least 1 letter and 1 number
+
+  username = '';
+  email = '';
+  password = '';
+
+
+
+  showEmailInput = false;
+  showPasswordInput = true;
+  showSubmitButton = false;
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+  }
+
+  onEmailChange() {
+    this.validateInputs();
+  }
+
+  onPasswordChange() {
+    this.validateInputs();
+  }
+
+  validateInputs() {
+    if (this.showPasswordInput) {
+      this.showSubmitButton = this.passwordPattern.test(this.password);
+    } else {
+      this.showSubmitButton = false;
+    }
+  }
 
 }
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export class SignUpComponent {
+export class SignUpComponent  implements OnInit, OnDestroy {
+  usernamePattern = /^[a-zA-Z]{2,}$/;
+  emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/; // min 6 chars, at least 1 letter and 1 number
+
+  username = '';
+  email = '';
+  password = '';
+
+
+  showNames = true;
+  showPasswordInput = true;
+  showSubmitButton = false;
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+  }
+
+  onDomainChange() {
+    this.validateInputs();
+  }
+
+  onEmailChange() {
+    this.validateInputs();
+  }
+
+  onPasswordChange() {
+    this.validateInputs();
+  }
+
+  validateInputs() {
+    if (this.showPasswordInput) {
+      this.showSubmitButton = this.passwordPattern.test(this.password);
+    } else {
+      this.showSubmitButton = false;
+    }
+  }
 
 }
